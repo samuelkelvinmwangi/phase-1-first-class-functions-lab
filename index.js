7f06f245496d66cad08725686e7e56debbe716f8
samuel <samuelkelvinmwangi@gmail.com>
2023-04-02 22:54:04 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(driver){
+    driver = drivers.splice(2, 0);
+    return driver;
+}
